452ac0baef5d9ef9bbba8b7569e658a7550d24bb
Magero Joshua <kyayijoshua@gmail.com>
2023-12-05 11:03:02 +0300

    edits

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Card from './components/Card';
 function App() {
   return (
     <div className="App">
+    
     <h1 className='underline text-lg font-bold'>Devjops Project</h1>
     {/* Navigation */}
       <Navigation />
